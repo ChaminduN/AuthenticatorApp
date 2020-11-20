b92f8f7276780857ff4f500cedc74c8c4a8b8582
ChaminduNisasala <33674197+ChaminduN@users.noreply.github.com>
2020-11-21 04:24:01 +0530

    Full Register and Login _Android_Studio

---
+++ app/src/androidTest/java/net/smallacademy/authenticatorapp/ExampleInstrumentedTest.java
@@ -0,0 +1,16 @@
+package net.smallacademy.authenticatorapp;
+
+import org.junit.Test;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+
+    }
+}
