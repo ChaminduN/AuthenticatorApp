f3bbdcaac877ab41bc71c63e595e862b76a3ddd2
ChaminduNisasala <33674197+ChaminduN@users.noreply.github.com>
2020-11-23 15:52:42 +0530

    After change xml files

---
+++ app/src/androidTest/java/com/example/constructo/ExampleInstrumentedTest.java
@@ -0,0 +1,11 @@
+package com.example.constructo;
+
+import org.junit.Test;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+
+
